1000690d90788970d1f60ee7752f45816f979c0d
b-yogesh <yogesh09121995@gmail.com>
2020-12-08 18:34:36 +0100

    test

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -20,7 +20,7 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+ 
 
 
 
